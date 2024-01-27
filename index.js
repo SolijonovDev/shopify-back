c9b1b02cda5fad386a6715c5cafe15befbb6f962
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2024-01-27 16:38:27 +0500

    cors removed

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import cors from 'cors';
 import routers from './routers/index.js';
 
 const app = express();
 const PORT = 4200;
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/api', routers);
