a8bd0233efea5898f9ec7975cee7e2e45b3179e1
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2024-01-27 16:40:44 +0500

    test

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import routers from './routers/index.js';
+// import routers from './routers/index.js';
 
 const app = express();
 const PORT = 4200;
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use('/api', routers);
+// app.use('/api', routers);
+
+app.get('/', (req, res) => {
+  res.send('all work');
+});
 
 const start = () => {
   app.listen(PORT, () => {
